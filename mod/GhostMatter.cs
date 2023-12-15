3c7d64c8a49a45a9277ae18721ff18aef8b8795b
ixrec <ericrhitchcock@gmail.com>
2023-12-15 05:21:50 +0000

    ghost matter take 2: remove misunderstanding of ObjectTrail PSRs and prevent scout from being an effortless replacement of this camera upgrade

--- mod/GhostMatter.cs
+++ mod/GhostMatter.cs
@@ -21,14 +21,7 @@ internal class GhostMatter
             var all_psrs = GameObject.FindObjectsOfType<ParticleSystemRenderer>();
             var wisp_psrs = all_psrs.Where(psr => psr.mesh?.name == "Effects_GM_WillOWisp");
 
-            // There is one WillOWisp PSR that's visible to the naked eye without taking a photo:
-            // In the Timber Hearth village's ghost matter tutorial area, every so often there's
-            // a visible green flash, and it leaves a little trail of smaller flashes behind it.
-            // That trail is a PSR named "ObjectTrail", and it's the only visible WillOWisp in the game.
-            // Anyway, we're not interested in editing that one, so that's why we do an isVisible filter here.
-            var invisible_wisp_psrs = wisp_psrs.Where(psr => !psr.isVisible);
-
-            ghostMatterParticleRenderers = invisible_wisp_psrs.ToList();
+            ghostMatterParticleRenderers = wisp_psrs.ToList();
         };
     }
 
@@ -42,17 +35,88 @@ internal class GhostMatter
 
             if (hasGhostMatterKnowledge)
             {
+                // todo: tweak notification if we also have scout already
                 var nd = new NotificationData(NotificationTarget.Player, "RECONFIGURING CAMERA TO CAPTURE GHOST MATTER WAVELENGTH", 10);
                 NotificationManager.SharedInstance.PostNotification(nd, false);
             }
         }
     }
 
+    // These two patches prevent camera photos from showing ghost matter until you have the item.
+
+    // The game enables and disables some of these particle renderers on its own at various times,
+    // so to ensure sure this works at all and avoid any weird side effects, we need to wait until
+    // the moment the camera is being used to actually disable the relevant renderers, *and* we
+    // need to re-enable the ones that were enabled before.
+    static List<ParticleSystemRenderer> disabledParticleRenderers = new();
+
     [HarmonyPrefix]
     [HarmonyPatch(typeof(ProbeLauncher), nameof(ProbeLauncher.TakeSnapshotWithCamera))]
     public static void ProbeLauncher_TakeSnapshotWithCamera_Prefix(ProbeCamera camera)
     {
-        foreach (var psr in ghostMatterParticleRenderers)
-            psr.enabled = hasGhostMatterKnowledge;
+        if (!hasGhostMatterKnowledge)
+        {
+            foreach (var psr in ghostMatterParticleRenderers)
+            {
+                psr.enabled = false;
+                disabledParticleRenderers.Add(psr);
+            }
+        }
+    }
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ProbeLauncher), nameof(ProbeLauncher.TakeSnapshotWithCamera))]
+    public static void ProbeLauncher_TakeSnapshotWithCamera_Postfix(ProbeCamera camera)
+    {
+        if (!hasGhostMatterKnowledge)
+        {
+            foreach (var psr in disabledParticleRenderers)
+            {
+                psr.enabled = true;
+            }
+            disabledParticleRenderers.Clear();
+        }
+    }
+
+    // This patch prevents the scout from showing "! Hazard" when it's inside ghost matter.
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HazardDetector), nameof(HazardDetector.GetDisplayDangerMarker))]
+    public static void HazardDetector_GetDisplayDangerMarker_Postfix(HazardDetector __instance, ref bool __result)
+    {
+        if (__instance._activeVolumes.All(av => av is HazardVolume && (av as HazardVolume).GetHazardType() == HazardVolume.HazardType.DARKMATTER))
+            __result = false;
+    }
+
+    // This patch prevents the scout from making a big green splash when it enters ghost matter.
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(HazardDetector), nameof(HazardDetector.OnVolumeAdded))]
+    public static void HazardDetector_OnVolumeAdded_Prefix(HazardDetector __instance, EffectVolume eVolume)
+    {
+        HazardVolume hazardVolume = eVolume as HazardVolume;
+        HazardVolume.HazardType hazardType = hazardVolume.GetHazardType();
+        Randomizer.Instance.ModHelper.Console.WriteLine($"HazardDetector.OnVolumeAdded {__instance.GetName()} {__instance.name} {eVolume.name} {hazardType} {__instance._darkMatterEntryEffect}");
+        if (__instance.GetName() == Detector.Name.Probe && hazardType == HazardVolume.HazardType.DARKMATTER)
+        {
+            Randomizer.Instance.ModHelper.Console.WriteLine($"HazardDetector_OnVolumeAdded_Prefix blocking the scout's darkMatterEntryEffect");
+            __instance._darkMatterEntryEffect = null;
+        }
+    }
+
+    // This patch prevents the scout from leaving a green trail as it passes through ghost matter.
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(DarkMatterVolume), nameof(DarkMatterVolume.OnEffectVolumeEnter))]
+    public static bool DarkMatterVolume_OnEffectVolumeEnter_Prefix(DarkMatterVolume __instance, GameObject hitObj)
+    {
+        HazardDetector detector = hitObj.GetComponent<HazardDetector>();
+        if (detector is null) return false; // no need to make the base game repeat this null check
+
+        // This prevents the scout from being added to the DMV's _trackedDetectors list,
+        // which is what it uses to emit the trail of WillOWisp particles as an object
+        // moves through ghost matter.
+        if (detector.GetName() == Detector.Name.Probe)
+        {
+            Randomizer.Instance.ModHelper.Console.WriteLine($"DarkMatterVolume_OnEffectVolumeEnter_Prefix blocking the scout's ghost matter particleTrail");
+            return false;
+        }
+        return true;
     }
 }
