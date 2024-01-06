24a6dc876095a99ead0787cfe5e61f1a7f661332
GameWyrm <daronan97@hotmail.com>
2024-01-06 00:37:13 -0700

    Removed unneccessary imports

--- mod/ArchConsoleManager.cs
+++ mod/ArchConsoleManager.cs
@@ -4,10 +4,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace ArchipelagoRandomizer
