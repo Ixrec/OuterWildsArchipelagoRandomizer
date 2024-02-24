12110328c8dbd8ff99b4b1faa4baee2b745c5f34
GameWyrm <daronan97@hotmail.com>
2024-02-24 14:01:56 -0700

    Internal reworking of logic calculation
    Should now be more efficient and I now have the ability to tell stats for accessibility in each area

---
+++ mod/InGameTracker/TrackerChecklistData.cs
@@ -0,0 +1,27 @@
+﻿namespace ArchipelagoRandomizer.InGameTracker
+{
+    public class TrackerChecklistData(bool isAccessible, bool hasBeenChecked, string hintItem, string hintReceiver)
+    {
+        /// <summary>
+        /// Whether the item has ever been accessible
+        /// </summary>
+        public bool isAccessible = isAccessible;
+        /// <summary>
+        /// Whether the location has been checked already
+        /// </summary>
+        public bool hasBeenChecked = hasBeenChecked;
+        /// <summary>
+        /// If a hint says something is here, this is the name of the item
+        /// </summary>
+        public string hintItem = hintItem;
+        /// <summary>
+        /// The player that owns the hinted item
+        /// </summary>
+        public string hintReceiver = hintReceiver;
+
+        public void SetAccessible(bool access)
+        {
+            isAccessible = access;
+        }
+    }
+}
