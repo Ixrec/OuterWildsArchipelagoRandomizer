a47a7ed8e1f46b8a62c36d34991d2a98fe901206
Ixrec <ericrhitchcock@gmail.com>
2024-03-05 13:31:19 +0000

    implement dcheck debug command for quickly checking locations

--- mod/ArchConsoleManager.cs
+++ mod/ArchConsoleManager.cs
@@ -298,6 +298,34 @@ namespace ArchipelagoRandomizer
                 consoleText.text = "";
                 return;
             }
+            if (text.StartsWith("!dcheck "))
+            {
+                var token = text.Substring("!dcheck ".Length);
+                uint count = uint.Parse(token);
+                APRandomizer.OWMLWriteLine($"Received debug command '{text}'.");
+
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        while (count > 0)
+                        {
+                            var cl = APRandomizer.Tracker.logic.GetLocationChecklist(InGameTracker.TrackerCategory.All);
+                            var uncheckedLocationName = cl.First(kv => !kv.Value.hasBeenChecked).Key;
+                            var uncheckedLocation = LocationNames.NameToLocation(uncheckedLocationName);
+                            LocationTriggers.CheckLocation(uncheckedLocation);
+
+                            count--;
+                            await Task.Delay(1000);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        APRandomizer.OWMLWriteLine($"dcheck exception: {e.Message}\n{e.StackTrace}");
+                    }
+                });
+                return;
+            }
 
             if (text == "") return;
 
