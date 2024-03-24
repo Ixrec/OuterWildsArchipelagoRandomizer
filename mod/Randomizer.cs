53689a25df6238d8308dd2e18c7fa074b35895f5
Ixrec <ericrhitchcock@gmail.com>
2024-03-24 21:36:12 +0000

    set mod version to 0.1.6(-rc) (including submodule update to 0.1.6)

--- mod/Randomizer.cs
+++ mod/Randomizer.cs
@@ -180,7 +180,7 @@ namespace ArchipelagoRandomizer
             {
                 var apworld_version = (string)loginSuccess.SlotData["apworld_version"];
                 // We don't take this from manifest.json because here we don't want the "-rc" suffix for Relase Candidate versions.
-                var mod_version = "0.1.5";
+                var mod_version = "0.1.6";
                 if (apworld_version != mod_version)
                     InGameAPConsole.AddText($"<color=red>Warning</color>: This Archipelago multiworld was generated with .apworld version <color=red>{apworld_version}</color>, " +
                         $"but you're playing version <color=red>{mod_version}</color> of the mod. This may lead to game-breaking bugs.");
