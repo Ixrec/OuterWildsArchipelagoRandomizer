45fac86980d40830347171a2afa5596fc866b951
Ixrec <ericrhitchcock@gmail.com>
2024-07-21 13:15:15 +0100

    fix SpawnPlayer() NRE at the Eye

--- mod/Spawn.cs
+++ mod/Spawn.cs
@@ -102,6 +102,11 @@ internal class Spawn
     [HarmonyPrefix, HarmonyPatch(typeof(PlayerSpawner), nameof(PlayerSpawner.SpawnPlayer))]
     public static bool PlayerSpawner_SpawnPlayer(PlayerSpawner __instance)
     {
+        if (LoadManager.GetCurrentScene() != OWScene.SolarSystem)
+        {
+            APRandomizer.OWMLModConsole.WriteLine($"PlayerSpawner_SpawnPlayer doing nothing, since we're not in the solar system");
+            return true; // let vanilla impl run
+        }
         if (spawnChoice == SpawnChoice.Vanilla || spawnChoice == SpawnChoice.TimberHearth)
         {
             APRandomizer.OWMLModConsole.WriteLine($"PlayerSpawner_SpawnPlayer doing nothing, since we're spawning in TH village");
