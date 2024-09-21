10e1b2030b9fd64d9e5a835d46d5b396e6c49316
Ixrec <ericrhitchcock@gmail.com>
2024-09-21 12:31:52 +0100

    work around 'infinite vortex bug'

--- mod/QuantumEntanglement.cs
+++ mod/QuantumEntanglement.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System;
 
 namespace ArchipelagoRandomizer;
 
@@ -37,6 +38,23 @@ internal class QuantumEntanglement
     {
         if (!hasEntanglementKnowledge)
             __result = false;
+    }
+    [HarmonyPostfix, HarmonyPatch(typeof(QuantumMoon), nameof(QuantumMoon.GetRandomStateIndex))]
+    public static void QuantumMoon_GetRandomStateIndex_Postfix(QuantumMoon __instance, ref int __result)
+    {
+        //APRandomizer.OWMLModConsole.WriteLine($"QuantumMoon_GetRandomStateIndex_Postfix {__result} / {hasEntanglementKnowledge} / {__instance.IsPlayerInside()}");
+        if (!hasEntanglementKnowledge && __instance.IsPlayerInside() && __result == 5)
+        {
+            // When the player is on the QM, without Entanglement Rule, and uses the Quantum Shrine to put themselves in darkness,
+            // usually the QM "leaves the player behind" as intended. But if the QM chooses to go to the 6th location,
+            // for some reason the player gets warped there and then trapped in the vortex above the south pole.
+            // The vortex infinite loops because of an early return condition in ChangeQuantumState() confused by my IsPlayerEntangled() patch.
+            // But I've been unable to find any trace of what is warping the player into the vortex in the first place.
+            // So in lieu of a direct fix, we're going to forbid the QM from choosing the 6th location in this state.
+
+            __result = UnityEngine.Random.Range(0, 4); // pick anything except 5 / the 6th location
+            APRandomizer.OWMLModConsole.WriteLine($"QuantumMoon_GetRandomStateIndex_Postfix worked around the infinite vortex bug by moving the Quantum Moon to state {__result} instead of 5");
+        }
     }
 
     static bool collidingWithQuantumObject = false;
