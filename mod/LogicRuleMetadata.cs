8c84623736b08ef71956b7c87a9936f39a649418
Crystal Fletcher <sporeluc@gmail.com>
2025-02-23 22:00:37 -0600

    Added "Feldspar Quick Access" logic option

---
+++ mod/LogicRuleMetadata.cs
@@ -0,0 +1,29 @@
+﻿using ArchipelagoRandomizer.InGameTracker;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArchipelagoRandomizer;
+
+public class LogicRuleMetadata
+{
+    public class LogicMetadata
+    {
+        public string trackerCategoryName; // shown directly to the user in the ship log / in-game tracker
+        public string slotDataOption; // the one you added to Options.py and __init__.py
+        public string logicCategory;  // the one you used in items.jsonc, locations.jsonc and connections.jsonc
+    };
+
+    private static LogicMetadata FeldsparQuickAccess = new LogicMetadata {
+        trackerCategoryName = "Feldspar via Quick Access",
+        slotDataOption = "feldspar_quick_access",
+        logicCategory = "feldspar_quick_access"
+    };
+
+    public static LogicMetadata[] AllLogicRules = {
+        FeldsparQuickAccess
+    };
+
+    public static Dictionary<string, LogicMetadata> LogicCategories = AllLogicRules.ToDictionary(rule => rule.logicCategory);
+}
+
+
