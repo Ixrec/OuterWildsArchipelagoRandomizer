9e48418fffda079ba31bacc6f83ee02a8b7ecf88
MYoshua64 <52865661+MYoshua64@users.noreply.github.com>
2025-06-07 17:28:07 +0300

    Outer Wilds: Additional Trap Items (#64)
    * Added Suit Puncture Trap
    
    * Privated variable
    
    * Added Map Disable Trap
    
    * Added HUD Corruption Trap
    
    * Updated inclusion of new traps in item entries
    
    * Added Ice Physics Trap
    
    * Added Supernova Trap
    
    * Update SupernovaTrap.cs
    
    Supernova shouldn't occur while in the Eye of the Universe scene
    
    * Minor fixes + commenting

---
+++ mod/ItemImpls/FillerAndTrap/IcePhysicsTrap.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+namespace ArchipelagoRandomizer
+{
+    [HarmonyPatch]
+    internal class IcePhysicsTrap
+    {
+        private static uint _icePhysicsTraps;
+
+        public static uint icePhysicsTraps
+        {
+            get => _icePhysicsTraps;
+            set
+            {
+                if (value > _icePhysicsTraps)
+                {
+                    ApplyIcePhysics();
+                }
+                _icePhysicsTraps = value;
+            }
+        }
+
+        private static void ApplyIcePhysics()
+        {
+            //we're in credits or menus. Ignore.
+            if (LoadManager.GetCurrentScene() != OWScene.SolarSystem && LoadManager.GetCurrentScene() != OWScene.EyeOfTheUniverse)
+                return;
+
+            //this call is necessary, otherwise the player is stuck in place and has to jump to have ice physics applied
+            characterController.MakeUngrounded();
+            icePhysicsApplied = true;
+        }
+
+        private static bool icePhysicsApplied;
+
+        private static PlayerCharacterController characterController = null;
+
+        [HarmonyPrefix, HarmonyPatch(typeof(PlayerCharacterController), nameof(PlayerCharacterController.Awake))]
+        private static void PlayerCharacterController_Awake(PlayerCharacterController __instance)
+        {
+            characterController = __instance;
+            icePhysicsApplied = false; //resetting this to avoid ice physics carrying over in the next loop
+        }
+
+        //CastForGrounded sets the collider and surface type the player is standing on.
+        //using a postfix to override that if we have ice physics applied for the duration of the loop
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerCharacterController), nameof(PlayerCharacterController.CastForGrounded))]
+        private static void PlayerCharacterController_CastForGrounded_Postfix()
+        {
+            if (!icePhysicsApplied)
+                return;
+
+            characterController._groundCollider.material.dynamicFriction = 0.0f;
+            characterController._groundSurface = SurfaceType.Ice;
+        }
+    }
+}
---
+++ mod/ItemImpls/FillerAndTrap/SupernovaTrap.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+
+namespace ArchipelagoRandomizer
+{
+    [HarmonyPatch]
+    internal class SupernovaTrap
+    {
+        private static uint _supernovaTraps;
+
+        public static uint supernovaTraps
+        {
+            get => _supernovaTraps;
+            set
+            {
+                if (value > _supernovaTraps)
+                {
+                    TriggerSupernova();
+                }
+                _supernovaTraps = value;
+            }
+        }
+
+        private static bool triggeredSupernovaInThisLoop = false;
+
+        private static void TriggerSupernova()
+        {
+            //trigger ONLY in the main solar system scene. Supernova while reaching the eye is not a good idea
+            if (LoadManager.GetCurrentScene() != OWScene.SolarSystem)
+                return;
+
+            //prevents triggering twice if a supernova is already in progress- whether by this trap of by the timeloop
+            if (triggeredSupernovaInThisLoop || TimeLoop.GetSecondsRemaining() <= 0.0)
+                return;
+
+            triggeredSupernovaInThisLoop = true;
+            GlobalMessenger.FireEvent("TriggerSupernova");
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(TimeLoop), nameof(TimeLoop.Awake))]
+        private static void TimeLoop_Awake_Prefix() => triggeredSupernovaInThisLoop = false;
+    }
+}
