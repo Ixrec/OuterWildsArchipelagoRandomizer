201ba718f11f0f9ae71979529c7bad782ef5cce4
RS_Mind <jared@awoodfamily.com>
2026-06-01 17:26:17 -0600

    Create FixRecursiveNode.cs
    The band-aid fix

---
+++ mod/ItemImpls/FCProgression/FixRecursiveNode.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+namespace ArchipelagoRandomizer.ItemImpls.FCProgression
+{
+    class FixRecursiveNode
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerSectorDetector), nameof(PlayerSectorDetector.OnAddSector))]
+        public static void PlayerSectorDetector_OnAddSector(PlayerSectorDetector __instance, Sector sector)
+        {
+            if (sector._idString != "Briar's Hollow") return;
+            // For an unknown reason, the recursive node in Briar's Hollow is sometimes disabled. We forcibly re-enable it here
+            GameObject loopNode = GameObject.Find("BriarsHollow_Body/Sector/Loop Node");
+            loopNode.SetActive(true);
+        }
+    }
+}
